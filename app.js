a218985a3a2309cb620a13f49505f12c670ed93c
Rohermy Ochoa <rohermy.ochoa@gmail.com>
2022-11-24 15:48:37 -0400

    add file .env

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-import  express  from "express"
+import * as dotenv from "dotenv"
+dotenv.config()
+import express from "express"
 
 const app = express();
 
+const PORT = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
   console.log("peticion recibida");
   res.status(200).send("<h1>holamundo</h1>")
 })
 
-app.listen(4000, () => {
-  console.log("servidor escuchando en puerto 4000")
+app.listen(PORT, () => {
+  console.log(`servidor escuchando en puerto ${PORT}`)
 })
+
+
