3e3e9019e14e412d9dd779e0d68181c335bc86b0
Rohermy Ochoa <rohermy.ochoa@gmail.com>
2022-11-24 16:51:11 -0400

    add javascript files

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
 import * as dotenv from "dotenv"
 dotenv.config()
 import express from "express"
 
 const app = express();
 
-const PORT = process.env.PORT || 4000
+
+//static files routes configure (middleware)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   console.log("peticion recibida");
-  res.status(200).send("<h1>holamundo</h1>")
+  console.log(__dirname);
+  res.status(200).sendFile("index.html", {root: __dirname})
 })
+
+//constante de entorno
+const PORT = process.env.PORT || 4000
+
+
 
 app.listen(PORT, () => {
   console.log(`servidor escuchando en puerto ${PORT}`)
