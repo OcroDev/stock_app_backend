b9dadadc6e36a1dbbf737e7a9991099186f1cec2
Rohermy <rohermy.ochoa@gmail.com>
2022-11-24 22:50:52 -0400

    conect database mongoDB

--- app.js
+++ app.js
@@ -1,30 +1,52 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
-import * as dotenv from "dotenv"
-dotenv.config()
-import express from "express"
+import path from "path";
+import { fileURLToPath } from "url";
+import * as dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import express from "express";
+import Colors from "colors";
 
 const app = express();
 
+//constante de entorno
+const PORT = process.env.PORT || 4000;
+
+//database conection
+mongoose
+  .connect(
+    `mongodb+srv://root:${process.env.MONGO_DB_PASS}@development.gmjmloq.mongodb.net/stock-app?retryWrites=true&w=majority`
+  )
+  .then((result) => {
+    console.log(Colors.bgGreen("conexion exitosa a la base de datos"));
+    app.listen(PORT, () => {
+      console.log(`servidor escuchando en puerto ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//esquema y modelo de la base de datos
+const productSchema = mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    price: { type: Number, require: true },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
 
 //static files routes configure (middleware)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-  console.log("peticion recibida");
-  console.log(__dirname);
-  res.status(200).sendFile("index.html", {root: __dirname})
-})
-
-//constante de entorno
-const PORT = process.env.PORT || 4000
-
-
-
-app.listen(PORT, () => {
-  console.log(`servidor escuchando en puerto ${PORT}`)
-})
-
-
+app.use(express.json());
+app.post("/api/v1/products", (req, res) => {
+  const newProduct = new Product(req.body);
+  newProduct
+    .save()
+    .then((result) => res.status(201).json({ ok: true }))
+    .catch((error) => console.log(error));
+});
--- public/index.js
+++ public/index.js
@@ -1,2 +1,21 @@
-const h1 = document.querySelector('h1')
-console.log(h1)
+const $inputName = document.getElementById("productName");
+const $inputPrice = document.getElementById("productPrice");
+console.log({ $inputName, $inputPrice });
+
+const $button = document.querySelector("button");
+
+console.log($button);
+
+$button.addEventListener("click", (e) => {
+  console.log({ name: $inputName.value, price: $inputPrice.value });
+  const name = $inputName.value;
+  const price = $inputPrice.value;
+  fetch("/api/v1/products", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: $inputName.value,
+      price: $inputPrice.value,
+    }),
+  });
+});
